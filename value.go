b65903867163fbfb0bd0d26522f485e382ab7841
Jeff Wendling <leterip@gmail.com>
2019-05-28 17:56:53 -0600

    add []string and setter interface

--- value.go
+++ value.go
@@ -1,65 +1,102 @@
 package structs
 
 import (
+	"encoding/csv"
 	"reflect"
+	"strings"
 	"time"
 
 	"github.com/spf13/cast"
 	"github.com/zeebo/errs"
 )
 
+type setter interface {
+	Set(s string) error
+}
+
 var ( // cached types for comparison
-	durationType = reflect.TypeOf(time.Duration(0))
-	timeType     = reflect.TypeOf(time.Time{})
+	durationType    = reflect.TypeOf(time.Duration(0))
+	timeType        = reflect.TypeOf(time.Time{})
+	setterType      = reflect.TypeOf((*setter)(nil)).Elem()
+	stringSliceType = reflect.TypeOf([]string(nil))
 )
 
 // setValue does the best job it can attempting to store the input into the output.
-func setValue(output reflect.Value, input interface{}) (bool, error) {
+func setValue(output reflect.Value, input interface{}) (set bool, err error) {
 	if !output.CanSet() {
 		return false, nil
 	}
 
 	var val interface{}
-	var err error
-
-	if typ := output.Type(); typ == durationType {
+	switch typ, kind := output.Type(), output.Kind(); {
+	// specific concrete types
+	case typ == durationType:
 		val, err = cast.ToDurationE(input)
-	} else if typ == timeType {
+	case typ == timeType:
 		val, err = cast.ToTimeE(input)
-	} else if kind := output.Kind(); kind == reflect.Bool {
+	case typ == stringSliceType:
+		sval, err := cast.ToStringE(input)
+		if err != nil {
+			return false, err
+		}
+		sval = strings.TrimPrefix(sval, "[")
+		sval = strings.TrimSuffix(sval, "]")
+		val, err = csv.NewReader(strings.NewReader(sval)).Read()
+
+	// if it can be set by string, do that
+	case typ.Implements(setterType):
+		sval, err := cast.ToStringE(input)
+		if err != nil {
+			return false, err
+		}
+		return true, output.Interface().(setter).Set(sval)
+	case output.CanAddr() && reflect.PtrTo(typ).Implements(setterType):
+		sval, err := cast.ToStringE(input)
+		if err != nil {
+			return false, err
+		}
+		return true, output.Addr().Interface().(setter).Set(sval)
+
+	// go by kind
+	case kind == reflect.Bool:
 		val, err = cast.ToBoolE(input)
-	} else if kind == reflect.Int {
+	case kind == reflect.Int:
 		val, err = cast.ToIntE(input)
-	} else if kind == reflect.Int8 {
+	case kind == reflect.Int8:
 		val, err = cast.ToInt8E(input)
-	} else if kind == reflect.Int16 {
+	case kind == reflect.Int16:
 		val, err = cast.ToInt16E(input)
-	} else if kind == reflect.Int32 {
+	case kind == reflect.Int32:
 		val, err = cast.ToInt32E(input)
-	} else if kind == reflect.Int64 {
+	case kind == reflect.Int64:
 		val, err = cast.ToInt64E(input)
-	} else if kind == reflect.Uint {
+	case kind == reflect.Uint:
 		val, err = cast.ToUintE(input)
-	} else if kind == reflect.Uint8 {
+	case kind == reflect.Uint8:
 		val, err = cast.ToUint8E(input)
-	} else if kind == reflect.Uint16 {
+	case kind == reflect.Uint16:
 		val, err = cast.ToUint16E(input)
-	} else if kind == reflect.Uint32 {
+	case kind == reflect.Uint32:
 		val, err = cast.ToUint32E(input)
-	} else if kind == reflect.Uint64 {
+	case kind == reflect.Uint64:
 		val, err = cast.ToUint64E(input)
-	} else if kind == reflect.Float32 {
+	case kind == reflect.Float32:
 		val, err = cast.ToFloat32E(input)
-	} else if kind == reflect.Float64 {
+	case kind == reflect.Float64:
 		val, err = cast.ToFloat64E(input)
-	} else if kind == reflect.String {
+	case kind == reflect.String:
 		val, err = cast.ToStringE(input)
-	} else if kind == reflect.Interface && reflect.TypeOf(input).Implements(output.Type()) {
+
+	// check interface matching
+	case kind == reflect.Interface && reflect.TypeOf(input).Implements(typ):
 		val = input
-	} else {
+
+	// ran out of options
+	default:
 		return false, errs.New("can't set input of type %T into output of type %v",
 			input, output.Type())
 	}
+
 	if err != nil {
 		return false, err
 	}
